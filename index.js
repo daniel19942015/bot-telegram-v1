71936ddba68ac595c45351ec9e1f5499a6cc22e1
Daniel Silva <daniel199257@gmail.com>
2020-08-18 20:06:50 -0300

    Logs

--- index.js
+++ index.js
@@ -11,11 +11,13 @@ const bot = new Telegraf(env.token);
 let user = '';
 
 bot.start(async context => {
+    console.log("============Saudacao===============")
     user = context.update.message.from.first_name;
     await context.reply(`Ola, ${user}.`)
     await context.reply(`Do que você gosta?`, tecladoOptions);
 })
 bot.hears(/astronomia/i, async context => {
+    console.log("============Interacao===============")
     await context.reply(`Olha, ${user}, eu também amo astronomia`);
     await context.reply(`Do que você gosta em astronomia? `, Markup.keyboard(
         ['Galáxias', 'Planetas', 'Estrelas', 'Buracos negros', 'Matéria escura']).resize().oneTime().extra()
@@ -23,6 +25,7 @@ bot.hears(/astronomia/i, async context => {
 })
 
 bot.hears(['Galáxias', 'Planetas'], async context => {
+    console.log("============Respostas===============")
     await context.reply(`Eu também amo ${context.match}.`)
 })
 
